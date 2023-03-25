de19a2410da561a620942d2cda9e172ae2b33012
treeculus <128884015+treeculus@users.noreply.github.com>
2023-03-25 11:46:00 +0530

    another

--- abc.c
+++ abc.c
@@ -1,11 +1,21 @@
 
 #include<stdio.h>
 int main(){
+  prin
+#include<stdio.h>tf("y
+#include<stdio.h>o");
+  printf("y
+#include<stdio.h>o");
+  
+  
+  
   printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
+  pri
+#include<stdio.h>ntf("y
+#include<stdio.h>o");
+  pri
+#include<stdio.h>ntf("yo");
+  pri
+#include<stdio.h>ntf("yo");
   return 0;
 }

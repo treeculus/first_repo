a926d243171add96391de664fd42e8e0d2049f5b
treeculus <kshitijs20@iitk.ac.in>
2023-03-25 06:21:01 +0000

    my first

--- abc.c
+++ abc.c
@@ -1,12 +1,22 @@
 
 #include<stdio.h>
 int main(){
+  prin
+#include<stdio.h>tf("y
+#include<stdio.h>o");
+  printf("y
+#include<stdio.h>o");
+  
+  
+  
   printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
-  printf("yo");
+  pri
+#include<stdio.h>ntf("y
+#include<stdio.h>o");
+  pri
+#include<stdio.h>ntf("yo");
+  pri
+#include<stdio.h>ntf("yo");
   return 0;
 }
 
